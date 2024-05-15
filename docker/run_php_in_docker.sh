afe12337343e9825380e1285f02cc9c5a12f4a49
Vadim Babadzhanyan <vadim.babadzhanyan@my.games>
2024-05-15 18:56:50 +0300

    test

--- docker/run_php_in_docker.sh
+++ docker/run_php_in_docker.sh
@@ -11,5 +11,4 @@ for ((argnum = 3; argnum <= $#; argnum++)); do
   ARGS="${ARGS}${!argnum} "
 done
 
-# echo "/usr/local/bin/docker compose -f ${HOST_DIR}/docker/docker-compose.yml exec -t php-fpm bash -c \"cd ${DOCKER_DIR}${SCRIPT_DIR} && php ${FILE_NAME} ${ARGS}\""
-/usr/local/bin/docker compose -f ${HOST_DIR}/docker/docker-compose.yml exec -t php-fpm bash -c "cd ${DOCKER_DIR}${SCRIPT_DIR} && php ${FILE_NAME} ${ARGS}"
+/usr/local/bin/docker compose -f "${HOST_DIR}/docker/docker-compose.yml" exec -t php-fpm bash -c "cd \"${DOCKER_DIR}${SCRIPT_DIR}\" && php \"${FILE_NAME}\" ${ARGS}"
